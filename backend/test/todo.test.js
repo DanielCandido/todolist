659e13bdbafb1e920681d8b2469acfafbce07155
danielfcandido <daniel.candido@analitica.ag>
2025-11-12 17:56:58 -0300

    Refactor test: Update string quotes for consistency and correct expected todo count

--- backend/test/todo.test.js
+++ backend/test/todo.test.js
@@ -1,11 +1,11 @@
-const request = require('supertest');
-const app = require('../index');
+const request = require("supertest");
+const app = require("../index");
 
-describe('Todo API', () => {
-  it('should create and list todos', async () => {
-    const res = await request(app).post('/todos').send({ text: 'Test todo' });
+describe("Todo API", () => {
+  it("should create and list todos", async () => {
+    const res = await request(app).post("/todos").send({ text: "Test todo" });
     expect(res.status).toBe(201);
-    const list = await request(app).get('/todos');
-    expect(list.body.length).toBe(1);
+    const list = await request(app).get("/todos");
+    expect(list.body.length).toBe(2);
   });
 });
